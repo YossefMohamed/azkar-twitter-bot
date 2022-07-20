1fe334c1b4e52648353fcffbc6b70c0a815d03ba
Yossef mohamed <47724276+YossefMohamed@users.noreply.github.com>
2022-07-20 12:43:51 +0200

    fix tweet time bug

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const app = express();
 const {client} = require("./handler/clientHandler");
 const axios = require('axios');
 const path = require('path');
-const delayTime = 60000 * 30 ;
+const delayTime = 60000 * 2 ;
 
 app.get("/", (_:any, res:any) => {
   res.send("<h1>Server Is Running :)</h1>")
@@ -41,7 +41,7 @@ const singleTweet = async () => {
 
 setInterval(() => {
   singleTweet();
-}, delayTime;
+}, delayTime);
 
 
 const port = process.env.PORT || 3000;
