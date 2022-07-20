37339a4f25790a9f7d5b48c6ca513a703b126cc0
Yossef mohamed <47724276+YossefMohamed@users.noreply.github.com>
2022-07-20 10:55:31 +0200

    finish bot using twitter api

---
+++ index.ts
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const {client} = require("./handler/clientHandler");
+const axios = require('axios');
+const path = require('path');
+const tenMinutesDelay = 60000 * 10 ;
+
+app.get("/", (_:any, res:any) => {
+  res.send("<h1>Server Is Running :)</h1>")
+})
+
+let hadithOrAyah = 1;
+const singleTweet = async () => {
+  let quranApi = 'https://api.alquran.cloud/ayah/';
+  try {
+    let ayahNumber = Math.floor(Math.random() * 6236) + 1
+    if(hadithOrAyah % 2 == 0){
+      const hadithNumber =  Math.floor(Math.random() * (300 - 2) + 2)
+      console.log(hadithNumber , "r");
+      let {data} = await axios(`https://api.hadith.sutanlab.id/books/bukhari?range=${hadithNumber}-${hadithNumber}`)
+      const {hadiths} = data.data
+      
+      console.log(data.data.hadiths[0].arab+ '\n \n' + "رواه البخاري");
+      await client.v1.tweet(hadiths[0].arab + '\n \n' + "رواه البخاري")
+      console.log("tweet successfully created")
+      hadithOrAyah++
+      return
+    }
+    let {data} = await axios(quranApi + ayahNumber + "/ar.asad")
+    
+    let numberOfAyahs = data.data.surah.numberOfAyahs;
+    let surah = await JSON.stringify([data.data.surah.name]);
+    let ayah = await JSON.stringify(data.data.text);
+    await client.v1.tweet(ayah + '\n \n' + surah + ' - ' + ' رقم الآيــة: '+ numberOfAyahs)
+    console.log("tweet successfully created")
+    return
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+
+setInterval(() => {
+  singleTweet();
+}, tenMinutesDelay);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("server is running on port 3000")
+});
